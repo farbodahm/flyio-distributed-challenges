079ec075256cd4682f6a51411ee32416a4927e17
Farbod Ahmadian <farbodahmadian2014@gmail.com>
2023-08-05 12:54:55 +0330

    Add challenge 4

---
+++ counter/main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"sync"
+	"time"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+// n is current node
+var n *maelstrom.Node
+var kv *maelstrom.KV
+var m sync.Mutex
+var visited_messages map[int]bool
+
+var value int
+
+// add_handler adds the new recieved value to the current state if
+// that massage id is not already seen.
+func add_handler(msg maelstrom.Message) error {
+	var body map[string]any
+	if err := json.Unmarshal(msg.Body, &body); err != nil {
+		return err
+	}
+
+	response := make(map[string]any)
+	response["type"] = "add_ok"
+
+	delta := int(body["delta"].(float64))
+	msg_id := int(body["msg_id"].(float64))
+
+	m.Lock()
+	if _, ok := visited_messages[msg_id]; ok {
+		m.Unlock()
+		return n.Reply(msg, response)
+	}
+
+	// TODO: Use that kv store
+	value += delta
+	visited_messages[msg_id] = true
+	m.Unlock()
+
+	go broadcast_message(delta, msg_id)
+
+	return n.Reply(msg, response)
+}
+
+// read_handler returns the current state.
+func read_handler(msg maelstrom.Message) error {
+	response := make(map[string]any)
+	response["type"] = "read_ok"
+	response["value"] = value
+
+	return n.Reply(msg, response)
+}
+
+// broadcast_message sends the message to all other nodes.
+// If it don't recieve an ack, it will try to send the message again.
+func broadcast_message(delta int, msg_id int) error {
+	nodes := make([]string, 0, len(n.NodeIDs())-1)
+	for _, node := range n.NodeIDs() {
+		if node != n.ID() {
+			nodes = append(nodes, node)
+		}
+	}
+
+	for len(nodes) > 0 {
+		node := nodes[0]
+		nodes = nodes[1:]
+
+		ctx, cancelFunc := context.WithTimeout(context.Background(), time.Second*2)
+		defer cancelFunc()
+
+		body := make(map[string]any)
+		body["type"] = "add"
+		body["delta"] = delta
+		body["msg_id"] = msg_id
+
+		// if couldn't receive ack, add value back to the queue
+		msg, err := n.SyncRPC(ctx, node, body)
+		if err != nil {
+			nodes = append(nodes, node)
+			continue
+		}
+		var response map[string]any
+		if err := json.Unmarshal(msg.Body, &response); err != nil {
+			return err
+		}
+		if response["type"] != "add_ok" {
+			log.Println("Unexpected response:", response)
+			return errors.New(fmt.Sprint("Unexpected response:", response))
+		}
+	}
+
+	return nil
+}
+
+func main() {
+
+	n = maelstrom.NewNode()
+	kv = maelstrom.NewSeqKV(n)
+	visited_messages = make(map[int]bool)
+
+	n.Handle("add", add_handler)
+	n.Handle("read", read_handler)
+
+	if err := n.Run(); err != nil {
+		log.Fatal(err)
+	}
+
+}
